8c4f297e595a11e045e451e58a42cf731ad64681
longtrinh1749 <trinhlong1749@gmail.com>
2021-05-20 16:05:31 +0700

    add cors again

--- app/app.js
+++ app/app.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 const app = express();
 
 app.use(bodyParser.json());
-app.use(cors);
+app.use(cors());
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", (req, res) => {
