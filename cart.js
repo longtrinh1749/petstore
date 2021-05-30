d5dae1ae0df83cb92a1c87a2543171b7c2ff2156
DrakeGoCoding <hoanglongphi21@gmail.com>
2021-05-31 00:31:19 +0700

    backend-control

---
+++ cart.js
@@ -0,0 +1,17 @@
+window.onload = function () {
+    let cart = getCart();
+    printCart(cart);
+}
+
+function getCart() {
+    let cart = getCookie('cart');
+    return JSON.parse(cart);
+}
+
+/**
+ * 
+ * @param {Array} cart 
+ */
+function printCart(cart){
+
+}
