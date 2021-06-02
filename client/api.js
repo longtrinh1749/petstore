3f81c6403abf3f54fae6a3482b00baebfc15af0b
NguyenThanhLong <long.nt184287@sis.hust.edu.vn>
2021-06-02 16:14:51 +0700

    add font and imgsrc to client

---
+++ client/api.js
@@ -0,0 +1,318 @@
+async function getAllProducts() {
+	let productList = []
+	try {
+		const res = await fetch('http://localhost:3000/items/all');
+		if (res) {
+			data = await res.json();
+			data.forEach(item => {
+				if (productList.findIndex(product => product.name.localeCompare(item.name) === 0) === -1) {
+					productList.push(item);
+				}
+			})
+		}
+	} catch (error) {
+		console.log(error);
+	}
+	return productList;
+}
+
+async function getProductsById(id) {
+	try {
+		const res = await fetch(`http://localhost:3000/items/getById?itemId=${id}`);
+		if (res) {
+			data = await res.json();
+			return data;
+		}
+		return [];
+	} catch (error) {
+		console.log(error);
+		return []
+	}
+}
+
+async function getProductsByName(name) {
+	let productList = []
+	try {
+		const res = await fetch(`http://localhost:3000/items/getByName?itemName=${name}`);
+		if (res) {
+			data = await res.json();
+			data.forEach(item => {
+				if (productList.findIndex(product => product.name.localeCompare(item.name) === 0) === -1) {
+					productList.push(item);
+				}
+			})
+			return productList;
+		}
+		return []
+	} catch (error) {
+		console.log(error);
+		return []
+	}
+}
+
+async function getProductByIdAndSize(id, size) {
+	try {
+		const res = await fetch(`http://localhost:3000/items/getByIdAndSize?itemId=${id}&itemSize=${size}`);
+		if (res) {
+			data = await res.json();
+			return data[0];
+		}
+		return null;
+	} catch (error) {
+		console.log(error);
+		return null;
+	}
+}
+
+async function postOrder(order) {
+	try {
+		const res = await fetch(`http://localhost:3000/items/form`, {
+			method: 'POST',
+			mode: 'cors',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify(order)
+		})
+		if (res) return true
+	} catch (error) {
+		console.log(error);
+		return false
+	}
+}
+
+function getAllSampleProducts() {
+	return [
+		{
+			id: "1",
+			imageurl: "ImgSource/shop_img_1.jpg",
+			name: "Collapsible Dog Bowl",
+			price: 45,
+			discount: 20,
+		},
+		{
+			id: "2",
+			imageurl: "ImgSource/shop_img_2.jpg",
+			name: "Lounger",
+			price: 85,
+			discount: 10,
+		},
+		{
+			id: "3",
+			imageurl: "ImgSource/shop_img_3.jpg",
+			name: "Active Pet Dog Harness",
+			price: 250,
+			discount: 30,
+		},
+		{
+			id: "4",
+			imageurl: "ImgSource/shop_img_4.jpg",
+			name: "Sara's Doggie Treat",
+			price: 45,
+			discount: 0,
+		},
+		{
+			id: "5",
+			imageurl: "ImgSource/shop_img_5.jpg",
+			name: "Little Scoop For Ultra",
+			price: 35,
+			discount: 0,
+		},
+		{
+			id: "6",
+			imageurl: "ImgSource/shop_img_6.jpg",
+			name: "Double Feeding Bowls",
+			price: 95,
+			discount: 0,
+		},
+		{
+			id: "7",
+			imageurl: "ImgSource/shop_img_7.jpg",
+			name: "Small Pet Drink Bottle",
+			price: 25,
+			discount: 0,
+		},
+		{
+			id: "8",
+			imageurl: "ImgSource/shop_img_8.jpg",
+			name: "Cat Scratch Apple Toy",
+			price: 90,
+			discount: 10,
+		},
+		{
+			id: "9",
+			imageurl: "ImgSource/shop_img_9.jpg",
+			name: "Mouse Cat Toy 10cm",
+			price: 55,
+			discount: 0,
+		},
+		{
+			id: "10",
+			imageurl: "ImgSource/shop_img_10.jpg",
+			name: "Dogs Name Tag",
+			price: 70,
+			discount: 0,
+		},
+		{
+			id: "11",
+			imageurl: "ImgSource/shop_img_11.jpg",
+			name: "Slow Feeder Bowl",
+			price: 50,
+			discount: 0,
+		},
+		{
+			id: "12",
+			imageurl: "ImgSource/shop_img_12.jpg",
+			name: "Dog Blanket With Cushion",
+			price: 90,
+			discount: 20,
+		},
+		{
+			id: "12",
+			imageurl: "ImgSource/shop_img_12.jpg",
+			name: "Dog Blanket With Cushion",
+			price: 90,
+			discount: 20,
+		},
+		{
+			id: "12",
+			imageurl: "ImgSource/shop_img_12.jpg",
+			name: "Dog Blanket With Cushion",
+			price: 90,
+			discount: 20,
+		},
+		{
+			id: "12",
+			imageurl: "ImgSource/shop_img_12.jpg",
+			name: "Dog Blanket With Cushion",
+			price: 90,
+			discount: 20,
+		},
+		{
+			id: "12",
+			imageurl: "ImgSource/shop_img_12.jpg",
+			name: "Dog Blanket With Cushion",
+			price: 90,
+			discount: 20,
+		},
+		{
+			id: "12",
+			imageurl: "ImgSource/shop_img_12.jpg",
+			name: "Dog Blanket With Cushion",
+			price: 90,
+			discount: 20,
+		},
+		{
+			id: "12",
+			imageurl: "ImgSource/shop_img_12.jpg",
+			name: "Dog Blanket With Cushion",
+			price: 90,
+			discount: 20,
+		},
+		{
+			id: "12",
+			imageurl: "ImgSource/shop_img_12.jpg",
+			name: "Dog Blanket With Cushion",
+			price: 90,
+			discount: 20,
+		},
+		{
+			id: "12",
+			imageurl: "ImgSource/shop_img_12.jpg",
+			name: "Dog Blanket With Cushion",
+			price: 90,
+			discount: 20,
+		},
+		{
+			id: "12",
+			imageurl: "ImgSource/shop_img_12.jpg",
+			name: "Dog Blanket With Cushion",
+			price: 90,
+			discount: 20,
+		},
+		{
+			id: "12",
+			imageurl: "ImgSource/shop_img_12.jpg",
+			name: "Dog Blanket With Cushion",
+			price: 90,
+			discount: 20,
+		},
+		{
+			id: "12",
+			imageurl: "ImgSource/shop_img_12.jpg",
+			name: "Dog Blanket With Cushion",
+			price: 90,
+			discount: 20,
+		},
+		{
+			id: "12",
+			imageurl: "ImgSource/shop_img_12.jpg",
+			name: "Dog Blanket With Cushion",
+			price: 90,
+			discount: 20,
+		},
+		{
+			id: "12",
+			imageurl: "ImgSource/shop_img_12.jpg",
+			name: "Dog Blanket With Cushion",
+			price: 90,
+			discount: 20,
+		},
+		{
+			id: "12",
+			imageurl: "ImgSource/shop_img_12.jpg",
+			name: "Dog Blanket With Cushion",
+			price: 90,
+			discount: 20,
+		},
+		{
+			id: "12",
+			imageurl: "ImgSource/shop_img_12.jpg",
+			name: "Dog Blanket With Cushion",
+			price: 90,
+			discount: 20,
+		},
+		{
+			id: "12",
+			imageurl: "ImgSource/shop_img_12.jpg",
+			name: "Dog Blanket With Cushion",
+			price: 90,
+			discount: 20,
+		},
+	];
+}
+
+function getSampleProducts() {
+	return [
+		{
+			description: 'The Pop Up Bowl was designed for convenience. Made of silicone, its collapsible design allows it to be flattened and extended with ease. This makes it ideal to carry with you on holiday or to the dog park!',
+			id: "1",
+			imageurl: "ImgSource/shop_img_1.jpg",
+			name: "Collapsible Dog Bowl",
+			size: 'M',
+			price: 45,
+			quantity: 27,
+			discount: 0,
+		},
+		{
+			description: 'The Pop Up Bowl was designed for convenience. Made of silicone, its collapsible design allows it to be flattened and extended with ease. This makes it ideal to carry with you on holiday or to the dog park!',
+			id: "1",
+			imageurl: "ImgSource/shop_img_1.jpg",
+			name: "Collapsible Dog Bowl",
+			size: 'L',
+			price: 46,
+			quantity: 30,
+			discount: 10,
+		},
+		{
+			description: 'The Pop Up Bowl was designed for convenience. Made of silicone, its collapsible design allows it to be flattened and extended with ease. This makes it ideal to carry with you on holiday or to the dog park!',
+			id: "1",
+			imageurl: "ImgSource/shop_img_1.jpg",
+			name: "Collapsible Dog Bowl",
+			size: 'XL',
+			price: 47,
+			quantity: 10,
+			discount: 15,
+		},
+	]
+}
---
+++ client/product.js
@@ -0,0 +1,151 @@
+var slideIndex = 1;
+var products = [];
+
+function plusSlides(n) {
+	showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+	showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+	var i;
+	var slides = document.getElementsByClassName("slide");
+	var dots = document.getElementsByClassName("dot");
+	if (n > slides.length) { slideIndex = 1 }
+	if (n < 1) { slideIndex = slides.length }
+	for (i = 0; i < slides.length; i++) {
+		slides[i].style.display = "none";
+	}
+	for (i = 0; i < dots.length; i++) {
+		dots[i].className = dots[i].className.replace(" active", "");
+	}
+	slides[slideIndex - 1].style.display = "block";
+	dots[slideIndex - 1].className += " active";
+}
+
+function addToCart(productId, productSize) {
+	const quantity = parseInt(document.querySelector('.product-description .product-quantity label input').value);
+	let cart = getCookie("cart");
+	let product;
+	cart = JSON.parse(cart);
+	if (!cart) {
+		product = { itemid: productId, size: productSize, quantity };
+		setCookie("cart", JSON.stringify([product]), 1);
+	} else if (Array.isArray(cart)) {
+		if (cart.length == 0) {
+			product = { itemid: productId, size: productSize, quantity };
+			setCookie("cart", JSON.stringify([product]), 1);
+		} else {
+			product = cart.find(item => item.itemid == productId && item.size == productSize);
+			if (!product) {
+				product = { itemid: productId, size: productSize, quantity };
+				setCookie("cart", JSON.stringify([...cart, product]), 1);
+			} else {
+				product.quantity = parseInt(product.quantity) + quantity;
+				setCookie("cart", JSON.stringify(cart), 1);
+			}
+		}
+	}
+	window.location.href = "cart.html";
+}
+
+function showProducts() {
+	let productContainer = document.getElementsByClassName('product-container')[0];
+	if (products.length === 0) {
+		productContainer.innerHTML = '<h1 style="color: red; font-weight: bold; text-align: center;">Error from server ...</h1>';
+		return;
+	}
+	productContainer.innerHTML = "";
+	productContainer.innerHTML += `
+		<div class="product-gallery">
+			<div class="slides">
+				<div class="slide fade">
+					<img src="${products[0].imageurl === "" ? "./ImgSource/shop_img_1.jpg" : products[0].imageurl}" alt="pd" style="width: 100%;">
+				</div>
+				<a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+				<a class="next" onclick="plusSlides(1)">&#10095;</a>
+			</div>
+			<div class="dots" style="text-align:center">
+				<span class="dot active" onclick="currentSlide(1)"></span>
+			</div>
+		</div>
+		<div class="product-description">
+			<h2 class="title">${products[0].name}</h2>
+			<div class="price">
+				${products[0].discount > 0 ? `<span>$${products[0].price.toFixed(2)}</span> $${(products[0].price - products[0].price * products[0].discount / 100).toFixed(2)}` : `$${products[0].price.toFixed(2)}`}
+			</div>
+			<p class="description">
+				${products[0].description}
+			</p>
+			<div class="product-highlight">
+				<h5><strong>Size</strong></h5>
+				<div class="custom-choose">
+				</div>
+			</div>
+			<div class="product-quantity">
+				<label for="quantity">
+					Quantity
+					<input type="number" name="quantity" min="1" max="${products[0].quantity}" value="1" oninput="checkQuantity(${products[0].quantity})">
+				</label>
+				<div class="add-to-cart">
+					Add To Cart
+					<i class="fas fa-shopping-bag"></i>
+				</div>
+			</div>
+		</div>
+	`
+	let customSizeDiv = productContainer.querySelector('.custom-choose');
+	for (let i = 0; i < products.length; i++) {
+		let p = products[i];
+		customSizeDiv.innerHTML += `
+			<input type="radio" id="${i}" value="${p.size}" name="size" onclick="changeSize(${i})" ${i == 0 ? "checked" : ""}>
+			<label for="${i}">${p.size}</label>
+		`
+	}
+	let addToCartBtn = document.querySelector('.product-description .product-quantity .add-to-cart')
+	addToCartBtn.addEventListener('click', function () {
+		addToCart(products[0].id, products[0].size)
+	})
+}
+
+function changeSize(index) {
+	let priceDiv = document.querySelector('.product-description .price');
+	let quantityInput = document.querySelector('.product-description .product-quantity label input');
+	let addToCartBtn = document.querySelector('.product-description .product-quantity .add-to-cart')
+
+	priceDiv.innerHTML = products[index].discount > 0
+		? `<span>$${products[index].price.toFixed(2)}</span> 
+		$${(products[index].price - products[index].price * products[index].discount / 100).toFixed(2)}`
+		: `$${products[index].price.toFixed(2)}`
+
+	quantityInput.setAttribute('max', products[index].quantity)
+	quantityInput.addEventListener('input', function () {
+		checkQuantity(products[index].quantity)
+	})
+	quantityInput.value = 1;
+	addToCartBtn.addEventListener('click', function () {
+		addToCart(products[index].id, products[index].size)
+	})
+}
+
+function checkQuantity(quantity) {
+	let quantityInput = document.querySelector('.product-description .product-quantity label input');
+	const value = quantityInput.value;
+	if (value > quantity) {
+		quantityInput.value = quantity;
+	} else if (value < 1) {
+		quantityInput.value = 1;
+	}
+}
+
+window.onload = async function () {
+	const queryString = window.location.search;
+	const urlParams = new URLSearchParams(queryString);
+	const id = urlParams.get('id');
+	products = await getProductsById(id);
+	// products = getSampleProducts();
+	showProducts();
+	showSlides(slideIndex);
+}
