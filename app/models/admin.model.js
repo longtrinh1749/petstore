8200e951f8f8e1d0dc9f2199d184ffe2fddd9bd8
longtrinh1749 <trinhlong1749@gmail.com>
2021-06-07 18:31:48 +0700

    update accounts.html

--- app/models/admin.model.js
+++ app/models/admin.model.js
@@ -3,6 +3,8 @@ const sql = require('./db');
 const Admin = function (admin) {
     this.username = admin.username;
     this.password = admin.password;
+    this.role = admin.role;
+    this.online = admin.online;
 };
 
 Admin.login = (username, password, result) => {
@@ -36,6 +38,106 @@ Admin.login = (username, password, result) => {
 
 Admin.logout = (username) => {
     sql.query("update admin set online = false where id = ?", [username]);
+}
+
+Admin.getAll = result => {
+    sql.query("select * from admin", (err, res) => {
+        if (err) {
+            console.log('Error: ', err);
+            result(err, null);
+            return;
+        }
+        // console.log('admins: ', res);
+        result(null, res);
+    });
+};
+
+Admin.get = (admin, result) => {
+    admin.username = '%' + admin.username + '%';
+    admin.password = '%' + admin.password + '%';
+    if(admin.role !== "ADMIN" && admin.role !== "EMPLOYEE") admin.role = "";
+    admin.role = '%' + admin.role + '%';
+    admin.online = '%' + admin.online + '%';
+    sql.query("select * from admin where id like N? and pw like N? and role like N? and online like N?;",
+        [admin.username, admin.password, admin.role, admin.online],
+        (err, res) => {
+            if (err) {
+                console.log("Err: ", err);
+                result(err, null);
+                return;
+            }
+            if (!res.length) {
+                result({kind: "not_found"}, null);
+                return;
+            } else {
+                console.log("Found admins: ", res);
+                result(null, res);
+                return;
+            }
+        })
+}
+
+Admin.create = (admin, result) => {
+    if(admin.role !== "ADMIN" && admin.role !== "EMPLOYEE") admin.role = "";
+    if(!admin.password) admin.password = "";
+    if(!admin.role) admin.role = "";
+    admin.online = false;
+    sql.query("insert into admin (`id`, `pw`, `role`, `online`) values (?, ?, ?, ?);", 
+        [admin.username, admin.password, admin.role, admin.online] ,
+        (err, res) => {
+            if (err) {
+                console.log("Err creating admin: ", err);
+                result(err, null);
+                return;
+            } else if (!res.affectedRows) {
+                console.log("Err create admin: Error in database (maybe duplicate id?).");
+                result({message: "Err create admin: Error in database (maybe duplicate id?)."}, null);
+                return;
+            } else if (res.affectedRows) {
+                console.log("Inserted admin into table");
+                result(null, res);
+                return;
+            }
+        });
+}
+
+Admin.update = (admin, result) => {
+    sql.query("update admin set pw = ?, role = ?, online = ?" +
+        "where id = ?", [admin.password, admin.role,
+        admin.online, admin.username], (err, res) => {
+        if (err) {
+            console.log("Err updating admin: ", err);
+            result(err, null);
+            return;
+        } else if (!res.affectedRows) {
+            console.log("Err update admin: Error in database (maybe cannot find id?).");
+            result({message: "Err create admin: Error in database (maybe cannot find id?)."}, null);
+            return;
+        } else if (res.affectedRows) {
+            console.log("Updated table admin");
+            result(null, res);
+            return;
+        }
+    });
+}
+
+Admin.delete = (admin, result) => {
+    sql.query("delete from admin where id = ?", [admin.username], (err, res) => {
+        console.log(admin.username);
+        if (err) {
+            console.log("Err deleting admin: ", err);
+            result(err, null);
+            return;
+        } else if (!res.affectedRows) {
+            console.log("Err delete admin: Error in database (maybe cannot find id?).");
+            result({message: "Err delete admin: Error in database (maybe cannot find id?)."}, null);
+            return;
+        } else if (res.affectedRows) {
+            console.log("Deleted from table admin");
+            result(null, res);
+            return;
+        }
+    })
 }
 
 module.exports = Admin;
--- app/routes/admin.account.routes.js
+++ app/routes/admin.account.routes.js
@@ -1,18 +1,111 @@
 const express = require('express');
 const session = require('express-session');
 const routes = express.Router();
-const Customer = require("../models/admin.model");
+const Admin = require("../models/admin.model");
 
 const path = require('../config')
 
 module.exports = routes;
 
-routes.get("/create", (req, res) => {
+routes.get("/all", (req, res) => {
+    Admin.getAll((err, data) => {
+        if(err) {
+            res.status(500).send({
+                message: err.message || "Some error occurred while get all item."
+            });
+        }
+        else res.send(data);
+    });
+});
+
+routes.post("/get", (req, res) => {
+    admin = req.body;
+    Admin.get(admin, (err, data) => {
+        if(err)
+            res.status(500).send({
+                message: err.message || "Some error occurred while get all item."
+            });
+        else res.send(data);
+    });
+});
+
+routes.get("/", (req, res) => {
+    if(req.session.admin) res.sendFile(path.views + "/admin/account.html");
+    else {
+        res.redirect("/admin");
+    }
+});
+
+routes.post("/create", (req, res) => {
     console.log("Creating");
-    if(req.session.role) {
-        console.log("Exist role in session");
+    if(req.session.admin) {
+        admin = req.body;
+        Admin.create(admin, (err, data) => {
+            if(err) {
+                if (err.kind === "not_found") {
+                    res.send({});
+                } else {
+                    res.status(500).send({
+                        message: err.message || "Some error occurred while updating admin."
+                    });
+                }
+            } else {
+                res.send();
+                console.log("Update success");
+            }
+        })
     }
     else {
         res.redirect("/admin");
     }
 });
+
+routes.post("/update", (req, res) => {
+    console.log("Updating");
+    if(req.session.admin) {
+        admin = req.body;
+        Admin.update(admin, (err, data) => {
+            if(err) {
+                if (err.kind === "not_found") {
+                    res.send({});
+                } else {
+                    res.status(500).send({
+                        message: err.message || "Some error occurred while updating admin."
+                    });
+                }
+            } else {
+                res.send();
+                console.log("Update success");
+            }
+        })
+    }
+    else {
+        res.redirect("/admin")
+        console.log("Lay file admin bang cach d nao co ?? :D ??");
+    }
+});
+
+routes.post("/delete", (req, res) => {
+    if(req.session.admin) {
+        admin = req.body;
+        Admin.delete(admin, (err, data) => {
+            if(err) {
+                if (err.kind === "not_found") {
+                    res.send({});
+                } else {
+                    res.status(500).send({
+                        message: err.message || "Some error occurred while updating admin."
+                    });
+                }
+            } else {
+                res.send(data);
+                console.log("Update success");
+            }
+        })
+    }
+    else {
+        res.redirect("/admin")
+        console.log("Lay file admin bang cach d nao co ?? :D ??");
+    }
+});
+
